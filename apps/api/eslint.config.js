01143c13bb59a0055c9090fcaf39b47fafc26c25
samuelcromwell <cromwellsamuel3@gmail.com>
2026-06-24 10:37:03 +0300

    fix(ci): include api tests in eslint project

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -10,7 +10,7 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: "./tsconfig.json"
+        project: "./tsconfig.eslint.json"
       },
       globals: {
         Buffer: "readonly",
